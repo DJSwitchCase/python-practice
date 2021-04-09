0c1d0945871bcfcecfb6b852240784891b5ab242
DJSwitchCase <lighterST@yandex.ru>
2021-04-09 18:21:02 +0300

    2

--- Practice2/main.py
+++ Practice2/main.py
@@ -1,3 +1,6 @@
+import numpy as np #
+import random
+import sys
 #ЛК
 '''
 help (max)
@@ -31,8 +34,8 @@ def zadanie1(s):
      for i in s:
          int(i)
          print(i, end=' ')
-     for i in s:
-         print(type(i), end = ' ')
+     #for i in s:
+        #print(type(i), end = ' ')
 
 
 # Подсчитать количество различных элементов в последовательности s.
@@ -62,14 +65,90 @@ def zadanie5(x):
 
 # Найти строку максимальной длины в списке строк s.
 def zadanie6(x):
-    pass
+    print(max(len(i) for i in x))
+
+
+# Пример работы *
+def get_multiple(*keys, dictionary, default=None):
+    return [
+        dictionary.get(key, default)
+        for key in keys
+    ]
+
+
+# изучение работы zip()
+def zipp():
+    s = 'abc'
+    t = (10, 20, 30)
+    print(zip(s, t))
+
+
+# генератор докладов
+def report_generator(n):
+    set1 = ['Коллеги,', 'В тоже время,', 'Однако,', 'Тем не менее', 'Следовательно,', 'Соответственно,',
+            'Вместе с тем', 'С другой стороны,']
+
+    set2 = ['парадигма цифровой экономики', 'контекст цифровой трансформации',
+            'диджитализация бизнес-процессов',
+            'прагматичный подход к цифровым платформам',
+            'совокупность сквозных технологий',
+            'программа прорывных исследований',
+            'ускорение блокчейн-транзакций',
+            'экспоненциальный рост Big Data']
+
+    set3 = ['открывает новые возможности для', 'выдвигает новые требования',
+            'несёт в себе риски', 'расширяет горизонты'
+            'заставляет искать варианты ', 'не оставляет шанса для' ,
+            'повышает вероятность', 'обостряет проблему']
+
+    set5 = ['знаний и компетенций.', 'непроверенных гипотез.',
+            'волатильных активов.', 'опасных экспериментов.',
+            'государственно-частных партнёрств.', 'цифровых следов граждан.',
+            'нежелательных последствий.', 'внезапных открытий.']
+    if n == 1: # для первого "Коллеги"
+        --n
+        yield set1[0]
+    else:
+        yield random.choice(list(set1))
+    yield random.choice(list(set2))
+    yield random.choice(list(set3))
+    yield random.choice(list(set5))
 
 
 s = ['1', '1', '2', '3', '4', '4', '5']
 x = [1, 7, 2, 3, 4, 5, 3, 2, 1, 1, 5, 6]
+z = ["privet", "kak dela", "alo1"]
 # zadanie1(s)
 # zadanie2(s)
 # zadanie3(s)
 # zadanie4(s, '4')
-zadanie5(x)
+# zadanie5(x)
+# zadanie6(z)
+# zipp()
+# для get_multiple()
+'''
+fruits = {'lemon': 'yellow', 'orange': 'orange', 'tomato': 'red'}
+print(get_multiple('lemon', 'tomtato', 'tomato', dictionary=fruits, default='unknown'))
+'''
+# transpose()
+s1 = ()
+a = [[0, 1, 2], [4, 5, 6]]
+a1 = [0, 1, 2]
+a2 = [4, 5, 6]
+# print(list(zip(a1, a2)))
+# a = np.array([[0, 1, 2], [4, 5, 6]])
+# print(a1, a2, sep="\n")
+# print(list(zip(a*)))
 
+# генератор докладов
+'''
+generator = report_generator(1)
+generator2 = report_generator(0)
+print(next(generator), next(generator), next(generator), next(generator),  sep=" ")
+print(next(generator2), next(generator2), next(generator2), next(generator2),  sep=" ")
+'''
+
+# print()
+print(25)
+sys.stdout.write(str(25) + '\n')
+print(25)
